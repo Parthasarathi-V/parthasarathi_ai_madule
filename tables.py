df60d9e4551484bb0497abfdcff2795ff5498f17
Parthasarathi-V 256 <123094596+Parthasarathi-V@users.noreply.github.com>
2024-01-24 07:31:14 +0530

    Update tables.py

--- tables.py
+++ tables.py
@@ -1,22 +1,24 @@
-def table(table, times, name):
-    if (name == "addition table"):
-        for i in range(1, times + 1):        
-            print(" {} + {}  =  {}".format(i, table, table+i))
+def table(tables, times, name):
+    for table in tables:
+        if (name == "addition table"):
+            for i in range(1, times + 1):        
+                print(" {} + {}  =  {}".format(i, table, table+i))
 
-    elif (name == "subtraction table"):
-        for i in range(1, times + 1):       
-            print(" {} - {}  =  {}".format(i, table, table-i))
+        elif (name == "subtraction table"):
+            for i in range(1, times + 1):       
+                print(" {} - {}  =  {}".format(i, table, table-i))
 
-    elif (name == "multiplication table"):
-        for i in range(1, times + 1):        
-            print(" {} × {}  =  {}".format(i, table, table*i))
-    
-    elif (name == "division table"):
-        for i in range(1, times + 1):        
-            print(" {} / {}  =  {}".format(i, table, table/i))
+        elif (name == "multiplication table" or name == "table"):
+            for i in range(1, times + 1):        
+                print(" {} × {}  =  {}".format(i, table, table*i))
+        
+        elif (name == "division table"):
+            for i in range(1, times + 1):        
+                print(" {} / {}  =  {}".format(i, table, table/i))
 
-    else:
-        print("\n***sorry, please check the input***")
+        else:
+            print("\n***sorry, please check the input***")
+        print()
 
 
 
