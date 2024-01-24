bf42f99192d892d79454117cab58b79a5a1981ab
Parthasarathi-V 256 <123094596+Parthasarathi-V@users.noreply.github.com>
2024-01-24 07:29:40 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,11 +3,11 @@ import converter
 
 
 #declaraction
-calc = ['add the', 'sub the', 'mul the', 'div the', 'the pow', 'subtract the', 'multiply the', 'divide the', 'the power']
-table = ['addition table', 'subtraction table', 'multiplication table', 'division table']
+calc = ['add the', 'sub the', 'mul the', 'div the', 'power', 'subtract the', 'multiply the', 'divide the', 'the power']
+table = ['addition table', 'subtraction table', 'multiplication table', 'division table' , 'table']
 
 #inputing statement
-query = input("").lower()
+query = input("What you want : ").lower()
 
 for i in calc:
     if i in query:
